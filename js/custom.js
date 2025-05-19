1939b0c630c8e619f17edc3c7c5f323626d49d9b
Emma A <classiradoo@gmail.com>
2025-05-19 19:23:51 -0400

    cleanup and carosel styling

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,35 @@
 
-  (function ($) {
-  
+(function ($) {
+
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+  });
 
-    // NAVBAR
-    $(".navbar").headroom();
+  // NAVBAR
+  $(".navbar").headroom();
 
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  $('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
-    $('.slick-slideshow').slick({
-      autoplay: true,
-      infinite: true,
-      arrows: false,
-      fade: true,
-      dots: true,
-    });
+  $('.slick-slideshow').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: false,
+    fade: true,
+    dots: true,
+  });
 
-    $('.slick-testimonial').slick({
-      arrows: false,
-      dots: true,
-    });
-    
-  })(window.jQuery);
+  $('.slick-testimonial').slick({
+    dots: true,
+    arrows: false,        // <-- This adds left/right arrows
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+})(window.jQuery);
